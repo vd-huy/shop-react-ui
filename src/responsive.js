9963cf45f63ea27cadc4e746a4f36ee507c82765
HuyKero0 <nhokvip004@gmail.com>
2023-08-05 16:07:16 +0700

    responsive

--- src/responsive.js
+++ src/responsive.js
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 export const mobile = (props) => {
     return css `
-    @media screen and (max-width: 320px) {
+    @media screen and (max-width: 380px) {
       ${props}
     }
   `;
